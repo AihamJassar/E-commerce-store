b76be9306a013b039dd9adbc3ece05a14c9f45c2
aiham jassar <aihamjassar@gmail.com>
2025-06-12 23:26:16 +0300

    chore: initialize package.json for e-commerce store with dependencies and scripts

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const { connectDB } = require("./db/config");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
